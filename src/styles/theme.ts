d0a420d1f9ca163efaae61d71be8a337daa256e8
Untest75 <khseok87n@gmail.com>
2022-09-27 22:57:34 +0900

    style: Add Common value

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,48 @@
+const bg = {
+  main: '#13141E',
+  sub: '#222426',
+  modal: '#5E5E74',
+};
+
 const colors = {
-  primary: '#444',
-  text: '#000',
-  textSecond: '#FFF',
+  main: {
+    purple: '#4E42C3',
+    green: '#2ED573',
+    blue: '#002868',
+  },
+  sub: {
+    blueGray: '#33354C',
+    gray: '#333333',
+    red: '#FF6040',
+  },
+  gray500: '#525252',
+  gray400: '#585858',
+  gray300: '#666666',
+  gray200: '#EDEDED',
+  gray100: '#FFFFFF',
+};
+
+const fontSize = {
+  xxxLarge: '40',
+  xxLarge: '32',
+  xLarge: '24',
+  large: '20',
+  medium: '16',
+  small: '14',
+};
+
+const fontWeight = {
+  regular: '400',
+  medium: '500',
+  semiBold: '600',
+  bold: '700',
 };
 
 const theme = {
+  bg,
   colors,
+  fontSize,
+  fontWeight,
 };
 
 export default theme;

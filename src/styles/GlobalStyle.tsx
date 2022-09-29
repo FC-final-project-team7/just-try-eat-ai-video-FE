fab0b8317c4525eae0ea809afc9b57e98395a65b
yejine2 <yejine2@naver.com>
2022-09-29 17:36:57 +0900

    feat: Add global font

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
   ${({ theme }) => css`
     * {
       box-sizing: border-box;
@@ -8,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
     html {
       background: ${theme.bg.main};
       color: ${theme.colors.gray100};
+      font-family: 'Noto Sans KR', sans-serif;
     }
   `}
 `;

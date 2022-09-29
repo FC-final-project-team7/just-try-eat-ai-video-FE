ae2dba9b6ce0de78ec28814a929790401fa83579
yejine2 <yejine2@naver.com>
2022-09-29 17:06:43 +0900

    feat: Add theme style

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,12 @@
 const bg = {
   main: '#13141E',
   sub: '#222426',
-  modal: '#5E5E74',
+  modal: '#292951',
 };
 
 const colors = {
   main: {
-    purple: '#4E42C3',
+    purple: '#5C4DF7',
     green: '#2ED573',
     blue: '#002868',
   },
@@ -14,28 +14,46 @@ const colors = {
     blueGray: '#33354C',
     gray: '#333333',
     red: '#FF6040',
+    purple: '#B1A9FF',
   },
-  gray500: '#525252',
-  gray400: '#585858',
-  gray300: '#666666',
-  gray200: '#EDEDED',
+  gray400: '#5E5D6D',
+  gray300: '#B0B0B0',
+  gray200: '#DEDDDD',
   gray100: '#FFFFFF',
 };
 
 const fontSize = {
-  xxxLarge: '40',
-  xxLarge: '32',
-  xLarge: '24',
-  large: '20',
-  medium: '16',
-  small: '14',
+  xxxLarge: '40px',
+  xxLarge: '32px',
+  xLarge: '24px',
+  large: '20px',
+  medium: '16px',
+  small: '14px',
 };
 
 const fontWeight = {
+  light: '300',
   regular: '400',
   medium: '500',
-  semiBold: '600',
   bold: '700',
+};
+
+const flex = {
+  flexCenter: `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  flexAround: `
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  `,
+  flexColumn: `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
 };
 
 const theme = {
@@ -43,6 +61,7 @@ const theme = {
   colors,
   fontSize,
   fontWeight,
+  flex,
 };
 
 export default theme;

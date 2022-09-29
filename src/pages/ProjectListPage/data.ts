5b29564599817bddeeaf179519f84a07d235ed47
yejine2 <yejine2@naver.com>
2022-09-29 16:47:47 +0900

    feat: Create dummy data

---
+++ src/pages/ProjectListPage/data.ts
@@ -0,0 +1,58 @@
+const PROJECT_DATA = [
+  {
+    projectId: 1,
+    name: '프로젝트명1',
+    createdDate: '2022-09-28',
+    thumbnail:
+      'https://velog.velcdn.com/images/yejine2/post/18bcdf7c-b3db-42ba-ac4a-00b37096501b/image.png',
+  },
+  {
+    projectId: 2,
+    name: '프로젝트명2',
+    createdDate: '2022-09-28',
+    thumbnail:
+      'https://velog.velcdn.com/images/yejine2/post/18bcdf7c-b3db-42ba-ac4a-00b37096501b/image.png',
+  },
+  {
+    projectId: 3,
+    name: '프로젝트명3',
+    createdDate: '2022-09-28',
+    thumbnail:
+      'https://velog.velcdn.com/images/yejine2/post/18bcdf7c-b3db-42ba-ac4a-00b37096501b/image.png',
+  },
+  {
+    projectId: 4,
+    name: '프로젝트명4',
+    createdDate: '2022-09-28',
+    thumbnail:
+      'https://velog.velcdn.com/images/yejine2/post/18bcdf7c-b3db-42ba-ac4a-00b37096501b/image.png',
+  },
+  {
+    projectId: 5,
+    name: '프로젝트명5',
+    createdDate: '2022-09-28',
+    thumbnail:
+      'https://velog.velcdn.com/images/yejine2/post/18bcdf7c-b3db-42ba-ac4a-00b37096501b/image.png',
+  },
+];
+
+const VIDEO_DATA = [
+  {
+    videoId: 1,
+    name: '영상1',
+    generated: false,
+    createdDate: '2022-10-01',
+    thumbnail:
+      'https://velog.velcdn.com/images/yejine2/post/d5ca1dc5-0279-46dd-9f00-a892e4d21fa0/image.png',
+  },
+  {
+    videoId: 2,
+    name: '영상2',
+    generated: false,
+    createdDate: '2022-10-01',
+    thumbnail:
+      'https://velog.velcdn.com/images/yejine2/post/d5ca1dc5-0279-46dd-9f00-a892e4d21fa0/image.png',
+  },
+];
+
+export { PROJECT_DATA, VIDEO_DATA };

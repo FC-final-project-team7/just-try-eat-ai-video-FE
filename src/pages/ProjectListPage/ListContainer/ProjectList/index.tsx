ebbb585969015e226a97fa583635eee58fa4e7be
yejine2 <yejine2@naver.com>
2022-09-30 00:50:24 +0900

    feat: Update Button

--- src/pages/ProjectListPage/ListContainer/ProjectList/index.tsx
+++ src/pages/ProjectListPage/ListContainer/ProjectList/index.tsx
@@ -3,13 +3,17 @@ import { PROJECT_DATA } from '~/pages/ProjectListPage/data';
 import ProjectCard from '../../Card/ProjectCard';
 
 import * as S from './style';
+import plus from '~/assets/icons/plus.svg';
 
 function ProjectList() {
   return (
     <S.ListBlock>
       <S.ListHeader>
         <S.ListTitle>내 프로젝트</S.ListTitle>
-        <S.button>+ 새 프로젝트</S.button>
+        <S.Button>
+          <img src={plus} alt="plus-icon" />
+          <S.ButtonText>새프로젝트</S.ButtonText>
+        </S.Button>
       </S.ListHeader>
       <S.ListCard>
         {PROJECT_DATA?.map((project) => (

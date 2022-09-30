0a6d6a61d862f6a803b4018a9e9f7b084d02d454
yejine2 <yejine2@naver.com>
2022-09-30 19:48:21 +0900

    feat: Update video card

--- src/pages/ProjectListPage/Card/VideoCard/index.tsx
+++ src/pages/ProjectListPage/Card/VideoCard/index.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import * as S from './style';
+import { TVideoData } from '~/types/projects';
+import { useState, useCallback } from 'react';
+import remove from '~/assets/icons/remove.svg';
 
-function VideoCard() {
-  return <div>생성된 영상 카드</div>;
+type Props = {
+  video: TVideoData;
+};
+
+function VideoCard(props: Props) {
+  const { video } = props;
+  const [showHoverBlock, setShowHoverBlock] = useState(false);
+
+  const onMouseOverHandler = useCallback(() => {
+    setShowHoverBlock(true);
+  }, [setShowHoverBlock]);
+
+  const onMouseOutHandler = useCallback(() => {
+    setShowHoverBlock(false);
+  }, [setShowHoverBlock]);
+
+  const onCardRemoveHandler = () => {
+    console.log('삭제 모달 띄우기');
+  };
+
+  return (
+    <S.CardBlock
+      onMouseOver={onMouseOverHandler}
+      onMouseOut={onMouseOutHandler}
+    >
+      <S.ImageBlock>
+        {video.thumbnail && <img src={video.thumbnail} alt="thumbnail" />}
+      </S.ImageBlock>
+      <S.ProjectInfo>
+        <S.ProjectTitle>{video.name}</S.ProjectTitle>
+        <S.ProjectDate>{video.createdDate}</S.ProjectDate>
+      </S.ProjectInfo>
+      {showHoverBlock && (
+        <>
+          <S.CardHoverBlock />
+          <S.CardHoverButtonBlock onClick={onCardRemoveHandler}>
+            <img src={remove} alt="remove-icon" />
+          </S.CardHoverButtonBlock>
+        </>
+      )}
+    </S.CardBlock>
+  );
 }
 
 export default VideoCard;

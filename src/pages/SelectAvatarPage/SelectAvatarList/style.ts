c0970ebf6fd6206db6756e5f7b0b1b7d64a1e1d2
doradora523 <doradora523@naver.com>
2022-10-01 03:57:51 +0900

    [Design]: Design the Select Avatar List by styled components

--- src/pages/SelectAvatarPage/SelectAvatarList/style.ts
+++ src/pages/SelectAvatarPage/SelectAvatarList/style.ts
@@ -1,1 +1,20 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 240px;
+  width: 680px;
+  height: 380px;
+  border: 1px solid #000;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-sizing: border-box;
+`;
+
+export const PreviewBox = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #08a689;
+`;

535f5e6176c0daa90b4d5f2d46343319b0dd05a4
yejine2 <yejine2@naver.com>
2022-10-01 22:33:23 +0900

    feat: Create ProjectSelectModal

---
+++ src/components/Popup/ProjectSelectModal/index.tsx
@@ -0,0 +1,55 @@
+import React, { Dispatch, SetStateAction, useRef } from 'react';
+import * as S from './style';
+import PopupPortal from '../PopupPortal';
+import cloud from '~/assets/icons/cloud.svg';
+import text from '~/assets/icons/text.svg';
+import close from '~/assets/icons/close.svg';
+
+import { pagesTo } from '~/pages/pages';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+};
+
+const ProjectSelectModal = (props: Props) => {
+  const { setShowModal } = props;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const onFileUploadHandler = () => {
+    inputRef.current?.click();
+  };
+
+  const onModalCloseHandler = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <PopupPortal>
+      <S.ModalBackground>
+        <S.ModalContainer>
+          <S.ModalTitle>음성을 보유하고 계신가요?</S.ModalTitle>
+          <S.ModalContent>
+            <S.VideoUploadButton onClick={onFileUploadHandler}>
+              <img src={cloud} alt="upload-icon" />
+              <S.ButtonText>음성 업로드하기</S.ButtonText>
+              <input type="file" style={{ display: 'none' }} ref={inputRef} />
+            </S.VideoUploadButton>
+            {/* // TODO 경로 수정 */}
+            <Link to={pagesTo.main}>
+              <S.VideoUploadButton>
+                <img src={text} alt="text-icon" />
+                <S.ButtonText>텍스트 입력으로 이동</S.ButtonText>
+              </S.VideoUploadButton>
+            </Link>
+          </S.ModalContent>
+          <S.ModalCloseButton onClick={onModalCloseHandler}>
+            <img src={close} alt="text-icon" />
+          </S.ModalCloseButton>
+        </S.ModalContainer>
+      </S.ModalBackground>
+    </PopupPortal>
+  );
+};
+
+export default ProjectSelectModal;
---
+++ src/components/Popup/ProjectSelectModal/style.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const ModalBackground = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 20;
+`;
+export const ModalContainer = styled.div`
+  ${(props) => props.theme.flex.flexColumn}
+  width: 928px;
+  height: 530px;
+  position: relative;
+  border-radius: 20px;
+  background-color: rgba(41, 41, 81, 0.8);
+  backdrop-filter: blur(15px);
+  padding: 84px 164px 88px;
+  gap: 32px;
+`;
+
+export const ModalTitle = styled.span`
+  font-size: ${(props) => props.theme.fontSize.xxLarge};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  width: 100%;
+  height: 60px;
+  gap: 40px;
+`;
+
+export const ModalCloseButton = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 48px;
+  cursor: pointer;
+`;
+
+export const VideoUploadButton = styled.div`
+  ${(props) => props.theme.flex.flexColumn}
+  width: 280px;
+  height: 280px;
+  background-color: ${(props) => props.theme.colors.main.purple};
+  border-radius: 10px;
+  padding-top: 52px;
+  cursor: pointer;
+  gap: 32px;
+`;
+
+export const ButtonText = styled.span`
+  font-size: ${(props) => props.theme.fontSize.medium};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+`;

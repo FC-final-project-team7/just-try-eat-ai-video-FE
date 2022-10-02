d196c8858d85423b7df23ec6a030c6477d75bc02
yejine2 <yejine2@naver.com>
2022-10-03 05:12:30 +0900

    feat: Add VideoDownladModal in VideoCard

--- src/pages/ProjectListPage/Card/VideoCard/index.tsx
+++ src/pages/ProjectListPage/Card/VideoCard/index.tsx
@@ -3,6 +3,7 @@ import { TVideoData } from '~/types/projects';
 import { useState, useCallback } from 'react';
 import remove from '~/assets/icons/remove.svg';
 import ConfirmModal from '~/components/Popup/ConfirmModal';
+import VideoDownloadModal from '~/components/Popup/VideoDownloadModal';
 
 type Props = {
   video: TVideoData;
@@ -12,6 +13,7 @@ function VideoCard(props: Props) {
   const { video } = props;
   const [showHoverBlock, setShowHoverBlock] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [showDownloadModal, setShowDownloadModal] = useState(false);
 
   const onMouseOverHandler = useCallback(() => {
     setShowHoverBlock(true);
@@ -25,33 +27,47 @@ function VideoCard(props: Props) {
     setShowConfirmModal(true);
   };
 
+  const onCardClickHandler = () => {
+    setShowDownloadModal(true);
+  };
+
   return (
-    <S.CardBlock
-      onMouseOver={onMouseOverHandler}
-      onMouseOut={onMouseOutHandler}
-    >
-      <S.ImageBlock>
-        {video.thumbnail && <img src={video.thumbnail} alt="thumbnail" />}
-      </S.ImageBlock>
-      <S.ProjectInfo>
-        <S.ProjectTitle>{video.name}</S.ProjectTitle>
-        <S.ProjectDate>{video.createdDate}</S.ProjectDate>
-      </S.ProjectInfo>
-      {showHoverBlock && (
-        <>
-          <S.CardHoverBlock />
-          <S.CardHoverButtonBlock onClick={onCardRemoveHandler}>
-            <img src={remove} alt="remove-icon" />
-          </S.CardHoverButtonBlock>
-        </>
-      )}
-      {showConfirmModal && (
-        <ConfirmModal
-          content="해당 프로젝트를 삭제할까요?"
-          setShowConfirmModal={setShowConfirmModal}
+    <>
+      <S.CardBlock
+        onMouseOver={onMouseOverHandler}
+        onMouseOut={onMouseOutHandler}
+        onClick={onCardClickHandler}
+      >
+        <S.ImageBlock>
+          {video.thumbnail && <img src={video.thumbnail} alt="thumbnail" />}
+        </S.ImageBlock>
+        <S.ProjectInfo>
+          <S.ProjectTitle>{video.name}</S.ProjectTitle>
+          <S.ProjectDate>{video.createdDate}</S.ProjectDate>
+        </S.ProjectInfo>
+        {showHoverBlock && (
+          <>
+            <S.CardHoverBlock />
+            <S.CardHoverButtonBlock onClick={onCardRemoveHandler}>
+              <img src={remove} alt="remove-icon" />
+            </S.CardHoverButtonBlock>
+          </>
+        )}
+        {showConfirmModal && (
+          <ConfirmModal
+            content="해당 프로젝트를 삭제할까요?"
+            setShowConfirmModal={setShowConfirmModal}
+          />
+        )}
+      </S.CardBlock>
+      {showDownloadModal && (
+        <VideoDownloadModal
+          setShowDownloadModal={setShowDownloadModal}
+          videoName={video.name}
+          videoDate={video.createdDate}
         />
       )}
-    </S.CardBlock>
+    </>
   );
 }
 

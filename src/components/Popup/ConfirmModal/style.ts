14e1ed857d38f0092f0e6923537ff3455fe3599a
yejine2 <yejine2@naver.com>
2022-10-02 17:57:05 +0900

    feat: Create ConfirmModal

---
+++ src/components/Popup/ConfirmModal/style.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import FilledButton from '~/components/Buttons/FilledButton';
+
+export const ModalBackground = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 20;
+`;
+export const ModalContainer = styled.div`
+  ${(props) => props.theme.flex.flexColumn}
+  width: 540px;
+  height: 360px;
+  border-radius: 20px;
+  background-color: rgba(41, 41, 81, 0.8);
+  backdrop-filter: blur(15px);
+  padding: 120px 98px 60px;
+  gap: 72px;
+`;
+export const ModalContent = styled.div`
+  width: 100%;
+  height: 60px;
+  font-size: ${(props) => props.theme.fontSize.large};
+  font-weight: ${(props) => props.theme.fontWeight.regular};
+  text-align: center;
+`;
+
+export const ModalButtonGroup = styled.div`
+  ${(props) => props.theme.flex.flexCenter}
+  gap: 32px;
+`;
+
+export const ModalButton = styled(FilledButton)`
+  font-size: ${(props) => props.theme.fontSize.medium};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+`;
+
+export const ModalButtonGray = styled(FilledButton)`
+  color: #5e5e74;
+  font-size: ${(props) => props.theme.fontSize.medium};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+  background-color: ${(props) => props.theme.colors.gray200};
+`;

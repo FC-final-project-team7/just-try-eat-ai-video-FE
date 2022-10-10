29ba50dcdb3bf16bbf2362abf40f01a45852c4ca
yejine2 <yejine2@naver.com>
2022-10-10 19:40:00 +0900

    feat: Update videoListApi url

--- src/stores/apis/videoList.ts
+++ src/stores/apis/videoList.ts
@@ -1,14 +1,11 @@
 import { emptySplitApiWithReauth } from '.';
-// import { projectsApi } from '.';
-// import { PROJECT_LIST_TAG } from './tags';
-
 import { TVideoData } from '~/types/projects';
 
 export const videoListApi = emptySplitApiWithReauth.injectEndpoints({
   endpoints: (build) => ({
     getVideos: build.query<TVideoData[], void>({
       query: () => ({
-        url: `/videos`,
+        url: `projects/videos`,
       }),
     }),
   }),

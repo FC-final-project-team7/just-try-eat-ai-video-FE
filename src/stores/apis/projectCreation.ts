62871a0f91ab7f316b15dc7f7155070f186c3659
yejine2 <yejine2@naver.com>
2022-10-12 02:34:57 +0900

    feat: Create projectCreationApi

---
+++ src/stores/apis/projectCreation.ts
@@ -0,0 +1,25 @@
+import { emptySplitApiWithReauth } from '.';
+import { PROJECT_LIST_TAG } from './tags';
+import { TProjectData } from '~/types/project/projects';
+
+export const projectCreationApi = emptySplitApiWithReauth.injectEndpoints({
+  endpoints: (build) => ({
+    textProject: build.mutation<TProjectData, void>({
+      query: () => ({
+        url: `/projects/text`,
+        method: 'POST',
+      }),
+      invalidatesTags: () => [{ type: PROJECT_LIST_TAG }],
+    }),
+    audioProject: build.query<{ projectId: number }, FormData>({
+      query: (formData) => ({
+        url: '/projects/audio',
+        method: 'POST',
+        body: formData,
+      }),
+    }),
+  }),
+});
+
+export const { useTextProjectMutation, useLazyAudioProjectQuery } =
+  projectCreationApi;
--- src/stores/apis/tags.ts
+++ src/stores/apis/tags.ts
@@ -1,2 +1,4 @@
 export const PROJECT_TEXT_TAG = 'ProjectText';
 export const PROJECT_SENTENCE_TAG = 'ProjectSentence';
+
+export const PROJECT_LIST_TAG = 'ProjectList';

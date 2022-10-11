05f46c760fd3ca22f71fa5c327a3d42cff64118e
yejine2 <yejine2@naver.com>
2022-10-12 02:35:50 +0900

    feat: Add deleteProject Api

--- src/stores/apis/projectList.ts
+++ src/stores/apis/projectList.ts
@@ -1,5 +1,6 @@
 import { emptySplitApiWithReauth } from '.';
 import { TProjectData } from '~/types/project/projects';
+import { PROJECT_LIST_TAG } from './tags';
 
 export const projectListApi = emptySplitApiWithReauth.injectEndpoints({
   endpoints: (build) => ({
@@ -7,8 +8,16 @@ export const projectListApi = emptySplitApiWithReauth.injectEndpoints({
       query: () => ({
         url: `/projects`,
       }),
+      providesTags: [{ type: PROJECT_LIST_TAG }],
+    }),
+    deleteProject: build.mutation<TProjectData, number | string>({
+      query: (id) => ({
+        url: `/projects/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: () => [{ type: PROJECT_LIST_TAG }],
     }),
   }),
 });
 
-export const { useGetProjectsQuery } = projectListApi;
+export const { useGetProjectsQuery, useDeleteProjectMutation } = projectListApi;

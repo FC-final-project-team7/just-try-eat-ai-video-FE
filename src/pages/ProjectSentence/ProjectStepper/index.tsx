55f88574e5a9da003af106b54851af924b31c251
Untest75 <khseok87n@gmail.com>
2022-10-14 18:03:06 +0900

    design: Apply contents margin

--- src/pages/ProjectSentence/ProjectStepper/index.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ProjectStepper = (props: Props) => {
-  return <div>ProjectTextStepper</div>;
-};
-
-export default ProjectStepper;
--- src/pages/ProjectText/ProjectTextStepper/index.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ProjectTextStepper = (props: Props) => {
-  return <div>ProjectTextStepper</div>;
-};
-
-export default ProjectTextStepper;
